35393244f4afc5f9cc8512c0ee499db6a8019869
PIERRON Marc <pierronmarc@outlook.fr>
2021-07-27 13:00:40 +0200

    feat: add router and bootstrap the project with a controller

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:8.0.8-apache
+FROM php:7.3.12-apache
 
 RUN a2enmod rewrite \
     && a2enmod headers\
--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
+require_once('../config/autoload.php');
 
-echo "Hello world";
+use Service\Router\Router;
+
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+include('../config/autoload.php');
+
+$router = new Router($_SERVER['REQUEST_URI']); 
+include('../config/routes.php');
+$router->run();
